9d3172d40d44aaff2520324b1fee34ac3a028c0e
Sam Baskin <sambskn@gmail.com>
2025-09-28 23:12:26 -0600

    working basic print of vals

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-// for las import, point cloud processing (and viz?)
-use threecrate::{Error, Point3f, PointCloud};
-use threecrate_io::read_point_cloud;
+// point cloud readin' son
+use las::Reader;
 
 // for arg handling
 use std::env;
-// for local file reads
-use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,25 +15,15 @@ fn main() {
     let filename = &args[1];
 
     println!("Opening point cloud .laz file at {filename}");
-
-    let laz_open_result = process_laz(filename);
-    if laz_open_result.is_err() {
-        let err = laz_open_result.unwrap_err();
-        println!("Error opening file! wtf???");
-        dbg!(err);
-    } else {
-        println!("Successfully read file!");
-        let point_cloud = laz_open_result.unwrap();
-        let point_count = point_cloud.points.len();
-        println!("yo got a point count {point_count}");
+    let mut reader = Reader::from_path(filename).expect("Couldn't open a reader");
+    let mut i = 0;
+    for point in reader.points() {
+        if i % 10000 == 0 {
+            let pnt = point.unwrap();
+            dbg!(pnt);
+        } 
+        i += 1;
     }
-
-    println!("all done! bye! :)");
+    println!("total count {i}");
 }
 
-fn process_laz(filename: &String) -> Result<PointCloud<Point3f>, Error> {
-    let full_path_str = &("./".to_string() + filename);
-    let path = Path::new(full_path_str);
-
-    read_point_cloud(path)    
-}
