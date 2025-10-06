608384aa3fd6d88e30d24ef9b0eda2ce9d1fe598
Sam Baskin <sambskn@gmail.com>
2025-10-05 20:22:09 -0600

    throw in a basic orbit cam

--- src/bevy_viz.rs
+++ src/bevy_viz.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 pub fn start_bevy() {
     App::new()
@@ -13,10 +14,44 @@ pub fn start_bevy() {
                 ..default()
             }),
         )
-        .add_systems(Update, hello_world_system)
+        .add_plugins(PanOrbitCameraPlugin)
+        .add_systems(Startup, setup_scene)
         .run();
 }
 
-fn hello_world_system() {
-    println!("sup it me da bevy app");
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // trying basic bevy demo 3d render
+
+    // circular base
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+
+    // camera
+    commands.spawn((
+        PanOrbitCamera::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
